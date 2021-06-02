cd05fcc5f9efc2adfc4a4f773fad6aa4bac52d0f
John Rojcewicz <rojcewiczj@gmail.com>
2021-06-02 14:25:56 -0500

    project setup intelliJ

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Java_Spring_PetClinic
+
 Building a web application for a fictional Veterinarians office in order to practice Java Spring skills.
---
+++ src/test/java/com/example/java_spring_petclinic/JavaSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
