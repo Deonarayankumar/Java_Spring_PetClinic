037a644c6a0c4d6244f3b06b1793b810a7255ba0
John Rojcewicz <rojcewiczj@gmail.com>
2021-06-02 15:18:29 -0500

    creating basic model classes

---
+++ src/main/java/com/example/java_spring_petclinic/model/PetType.java
@@ -0,0 +1,14 @@
+package com.example.java_spring_petclinic.model;
+
+public class PetType {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/example/java_spring_petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.example.java_spring_petclinic.model;
+
+public class Vet extends Person{
+}
