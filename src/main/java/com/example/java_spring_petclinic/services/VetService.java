39649bce79a7512e3157034d8b1dbbb2290ffef7
John Rojcewicz <rojcewiczj@gmail.com>
2021-06-02 15:40:19 -0500

    created service interfaces

---
+++ src/main/java/com/example/java_spring_petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package com.example.java_spring_petclinic.services;
+
+import com.example.java_spring_petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
